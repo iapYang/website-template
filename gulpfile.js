a84117d28dc77ef5a160d353d6137bad0e00520e
ヨシュア <yss901025@gmail.com>
2015-11-14 15:55:37 +0800

    u

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,69 @@
 var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
 var minifyCss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
-var clean = require('gulp-clean');
 var imagemin = require('gulp-imagemin');
+var changed = require('gulp-changed');
+var clean = require('gulp-clean');
+var filter = require('gulp-filter');
+
+var sourceHtmlPath = 'dev/**';
+var sourceCssPath = 'dev/css/**';
+var sourceJsPath = 'dev/js/**';
+var sourceImagePath = 'dev/img/**';
+
+var distHtmlPath = 'dist';
+var distCssPath = 'dist/css';
+var distJsPath = 'dist/js';
+var distImagePath = 'dist/img';
+
+var htmlFilter = filter(['**/*.html', '!vendors/**'], {restore: true});
+var cssFilter = filter(['**/*.css', '!vendors/**'], {restore: true});
+var jsFilter = filter(['**/*.js', '!vendors/**'], {restore: true});
+var imageFilter = filter(['**/*.{png,jpg,jpeg,gif}', '!vendors/**'], {restore: true});
+
+
+gulp.task('minify-html', function(){
+    return gulp.src(sourceHtmlPath)
+    .pipe(htmlFilter)
+    .pipe(changed(distHtmlPath))
+    .pipe(htmlmin({
+        removeComments: true,
+        collapseWhitespace: true
+    }))
+    .pipe(gulp.dest(distHtmlPath));
+});
 
 gulp.task('minify-css', function(){
-    return gulp.src('dev/css/*.css')
+    return gulp.src(sourceCssPath)
+    .pipe(cssFilter)
+    .pipe(changed(distCssPath))
     .pipe(minifyCss())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest(distCssPath));
 });
 
 gulp.task('minify-js', function () {
-    return gulp.src('dev/build/*.js')
+    return gulp.src(sourceJsPath)
+    .pipe(jsFilter)
+    .pipe(changed(distJsPath))
     .pipe(uglify())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest(distJsPath));
 });
+
+gulp.task('minify-image', function(){
+    return gulp.src(sourceImagePath)
+    .pipe(imageFilter)
+    .pipe(imagemin())
+    .pipe(gulp.dest(distImagePath));
+});
+
+
 
 gulp.task('clean', function(){
     return gulp.src('dist')
     .pipe(clean());
 });
 
-gulp.task('imagemin', function(){
-    return gulp.src('dev/img/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/img'));
+gulp.task('watch', function(){
+    gulp.watch('dev/css/*.css', ['minify-css']);
 });
