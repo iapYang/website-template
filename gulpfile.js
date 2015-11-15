4d2048024e3340aace6a435296c694c3dda7ab7e
ヨシュア <yss901025@gmail.com>
2015-11-15 14:26:24 +0800

    u

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var minifyCss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var stripDebug = require('gulp-strip-debug');
 var imagemin = require('gulp-imagemin');
+var copy = require('gulp-copy');
 var filter = require('gulp-filter');
 var changed = require('gulp-changed');
 var clean = require('gulp-clean');
@@ -14,28 +15,35 @@ var zip = require('gulp-zip');
 var sourcePath = 'dev';
 var distPath = 'dist';
 
-var htmlPath = '/';
-var cssPath = '/css';
-var jsPath = '/js';
-var imgPath = '/img';
+var htmlPath = '';
+var cssPath = 'css';
+var jsPath = 'js';
+var imgPath = 'img';
 
-var fullPath = '/**';
+var singlePath = '*';
+var fullPath = '**';
 
-var sourceHtmlPath = sourcePath + fullPath;
-var sourceCssPath = sourcePath + cssPath + fullPath;
-var sourceJsPath = sourcePath + jsPath + fullPath;
-var sourceImgPath = sourcePath + imgPath + fullPath;
+var archivePath = './';
+var archiveName = 'archive';
+var archiveExt = '.zip';
 
-var distHtmlPath = distPath + htmlPath;
-var distCssPath = distPath + cssPath;
-var distJsPath = distPath + jsPath;
-var distImgPath = distPath + imgPath;
+var sourceAllPath = [sourcePath, fullPath].join('/');
+var sourceHtmlPath = [sourcePath, htmlPath, singlePath].join('/');
+var sourceCssPath = [sourcePath, cssPath, fullPath].join('/');
+var sourceJsPath = [sourcePath, jsPath, fullPath].join('/');
+var sourceImgPath = [sourcePath, imgPath, fullPath].join('/');
+
+var distAllPath = [distPath, fullPath].join('/');
+var distHtmlPath = [distPath, htmlPath].join('/');
+var distCssPath = [distPath, cssPath].join('/');
+var distJsPath = [distPath, jsPath].join('/');
+var distImgPath = [distPath, imgPath].join('/');
 
 var htmlFilter = filter(['**/*.html', '!vendors/**'], {restore: true});
 var cssFilter = filter(['**/*.css', '!vendors/**'], {restore: true});
 var jsFilter = filter(['**/*.js', '!vendors/**'], {restore: true});
 var imageFilter = filter(['**/*.{png,jpg,jpeg,gif}', '!vendors/**'], {restore: true});
-
+var otherFilter = filter(['**/*', '!*.html', '!{css,js,img,sass,build,vendors}/**'], {restore: true});
 
 
 gulp.task('minify-html', function(){
@@ -73,25 +81,29 @@ gulp.task('minify-img', function(){
     .pipe(gulp.dest(distImgPath));
 });
 
-
+gulp.task('copy', function(){
+    return gulp.src(sourceAllPath)
+    .pipe(otherFilter)
+    .pipe(copy(distPath, {
+        prefix: 1
+    }));
+});
 
 gulp.task('clean', function(){
-    return gulp.src(distPath)
+    return gulp.src([distPath, archiveName, archivePath.concat(archiveName, archiveExt)])
     .pipe(clean());
 });
 
 gulp.task('compress', function(){
-    return gulp.src(distPath + fullPath)
-    .pipe(zip('archive.zip'))
-    .pipe(gulp.dest('.'));
+    return gulp.src(distAllPath)
+    .pipe(zip(archiveName.concat(archiveExt)))
+    .pipe(gulp.dest(archivePath));
 });
 
-gulp.task('watch', function(){
-    gulp.watch('dev/css/*.css', ['minify-css']);
-});
-
-
+// gulp.task('watch', function(){
+//     gulp.watch('dev/css/*.css', ['minify-css']);
+// });
 
 gulp.task('build', function(cb){
-    sequence('clean', ['minify-html', 'minify-css', 'minify-js', 'minify-img'], 'compress', cb);
+    sequence('clean', ['minify-html', 'minify-css', 'minify-js', 'minify-img'], 'copy', 'compress', cb);
 });
