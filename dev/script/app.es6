7a20c5d15cc854066aa749c6afd58800632566fb
ヨシュア <yss901025@gmail.com>
2016-02-26 17:29:55 +0800

    add picture.js

--- dev/script/app.es6
+++ dev/script/app.es6
@@ -1,8 +1,12 @@
 var $ = require('jquery');
 var TweenMax = require('gsap');
+var Picture = require('./util/picture.js');
 
-TweenMax.to('.container', 0.5, {
-    x: 100,
-    y: 200,
-    scale: 2
+Picture.preload({
+    load: function(count, total){
+        console.log('==========', count, total);
+    },
+    end: function(){
+        console.log('==========end');
+    }
 });
---
+++ dev/script/util/picture.js
@@ -0,0 +1,76 @@
+(function(factory){
+    if ( typeof module === 'object' && typeof module.exports === 'object' ){
+            console.log('load with commonJS');
+            module.exports = factory();
+    }else{
+        console.log('load with normal');
+        factory();
+    }
+}(function(){
+    var className = 'preload';
+    var dataName = 'data-source';
+
+    var items;
+    var loadCount = 0;
+    var totalCount;
+    var options;
+
+    var defaultOptions = {
+        load: function(){},
+        end: function(){}
+    }
+
+    var Picture = function(){
+
+    }
+
+    Picture.preload = function(opts){
+        options = Object.assign({}, defaultOptions, opts);
+
+        items = document.getElementsByClassName(className);
+        totalCount = items.length;
+
+        if(totalCount == 0){
+            doEnd();
+        }
+
+        for(var i = 0; i < totalCount; ++i){
+            startLoad(items[i]);
+        }
+    }
+
+    function startLoad(item){
+        var src = item.getAttribute(dataName);
+        var image = new Image();
+
+        image.onload = function(){
+            item.appendChild(this);
+            doLoad();
+        };
+        image.onerror = function(){
+            doLoad();
+        }
+
+        image.src = src;
+    }
+
+    function doLoad(){
+        ++loadCount;
+
+        options.load(loadCount, totalCount);
+
+        if(loadCount == totalCount){
+            doEnd();
+        }
+    }
+
+    function doEnd(){
+        options.end(loadCount, totalCount);
+    }
+
+
+
+    window.Picture = Picture;
+
+    return Picture;
+}));
