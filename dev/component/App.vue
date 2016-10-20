661c60c6afbd00ec49f196466c360e75f1aab1e5
ヨシュア <yss901025@gmail.com>
2016-10-20 12:09:46 +0800

    去除多余的注入

--- dev/component/App.vue
+++ dev/component/App.vue
@@ -6,13 +6,10 @@
 </template>
 
 <script>
-import store from '../vuex/store';
-
 import Message from './Message.vue';
 import Controller from './Controller.vue';
 
 export default {
-    store,
     components: {
         Message,
         Controller,
