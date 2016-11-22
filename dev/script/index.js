20a9e20f7af9f4366355f5d9bdf692d89c3a1e0b
ヨシュア <yss901025@gmail.com>
2016-11-22 21:49:00 +0800

    uuu

--- dev/script/index.js
+++ dev/script/index.js
@@ -1,17 +1,45 @@
 import 'babel-polyfill';
 
-import Vue from 'vue';
-
-import router from '../router';
-import store from '../store/index';
-import App from '../component/App.vue';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 
+class Clock extends React.Component{
+    constructor(props){
+        super(props);
 
-const app = new Vue({
-    router,
-    store,
-    components: {
-        App
-    },
-}).$mount('#app');
+        this.state = {
+            date: new Date(),
+        }
+    }
+
+    componentDidMount(){
+        this.timmer = setInterval(() => {
+            this.tick();
+        }, 1000);
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.timmer);
+    }
+
+    tick(){
+        this.setState({
+            date: new Date(),
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <h1 className={this.props.className}>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Clock className="tttaaaaa" />,
+    document.getElementById('app')
+);
