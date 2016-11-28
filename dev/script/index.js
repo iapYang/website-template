a83061344fac945b17905ae074889bf574437244
ヨシュア <yss901025@gmail.com>
2016-11-28 16:45:48 +0800

    navigation

--- dev/script/index.js
+++ dev/script/index.js
@@ -3,130 +3,53 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function calculateWinner(squares) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-        const [a,
-            b,
-            c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
-    }
+import '../style/index.scss';
 
-    return null;
-}
-
-class Square extends React.Component {
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={() => this.props.onClick()}>
-                {this.props.value}
-            </button>
-        );
-    }
-}
-
-class Board extends React.Component {
+class Navigation extends React.Component {
     constructor() {
         super();
 
         this.state = {
-            squares: new Array(9).fill(null),
-            xIsNext: true,
+            items: [
+                'home',
+                'projects',
+                'services',
+                'concat',
+            ],
+            activeIndex: 0,
         };
     }
-    handleClick(i) {
-        const squares = [...this.state.squares];
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
+    clickHandler(i) {
         this.setState({
-            squares,
-            xIsNext: !this.state.xIsNext,
+            activeIndex: i,
         });
     }
-    renderSquare(i) {
-        return <Square
-            value={this.state.squares[i]}
-            onClick={() => this.handleClick(i)} />;
-    }
     render() {
-        const winner = calculateWinner(this.state.squares);
-        let status;
+        const lis = this.state.items.map((item, i) => {
+            const className = this.state.activeIndex === i ? 'active' : '';
 
-        if (winner) {
-            status = `Winner: ${winner}`;
-        } else {
-            status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
-        }
+            return <li key={i}
+                className={className}
+                onClick={() => this.clickHandler(i)}>{item}</li>;
+        });
+
 
         return (
-            <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+            <div className="navigation">
+                <ul>
+                    {lis}
+                </ul>
+
+                <h2 className="choice">
+                    You chose
+                    <span>
+                         {this.state.items[this.state.activeIndex]}
+                    </span>
+                </h2>
             </div>
         );
     }
 }
-
-class Game extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            history: [{
-                squares: new Array(9).fill(null),
-                xIsNext: true,
-            }],
-        };
-    }
-
-    render() {
-        return (
-            <div className="game">
-                <div className="game-board">
-                    <Board/>
-                </div>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
-            </div>
-        );
-    }
-}
-
-// ========================================
 
 ReactDOM.render(
-    <Game/>,
-    document.getElementById('app')
-);
+    <Navigation></Navigation>, document.getElementById('app'));
