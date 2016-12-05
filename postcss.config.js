b6cc8151163a31390745e06a56d7c3dfc31a5bda
ヨシュア <yss901025@gmail.com>
2016-12-05 15:23:26 +0800

    完善格式

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  plugins: [
-      require('postcss-cssnext')(),
-      require('postcss-sorting')()
-  ]
+    plugins: [
+        require('postcss-cssnext')(),
+        require('postcss-sorting')(),
+    ],
 };
