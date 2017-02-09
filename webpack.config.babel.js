bc5e2714081b1ae44b32d229ec363c7ac539e8db
ヨシュア <yss901025@gmail.com>
2017-02-09 09:33:29 +0800

    add OccurenceOrderPlugin & DedupePlugin

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -98,6 +98,8 @@ if (process.env.NODE_ENV === 'production') {
     webpackConfig = merge(baseWebpackConfig, {
         plugins: [
             ...htmlPlugins,
+            new webpack.optimize.OccurenceOrderPlugin(),
+            new webpack.optimize.DedupePlugin(),
             new webpack.optimize.UglifyJsPlugin({
                 compress: {
                     warnings: false,
