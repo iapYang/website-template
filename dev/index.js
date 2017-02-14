e47e58343eb542b7e471d08dc06102a65e0756ac
ヨシュア <yss901025@gmail.com>
2017-02-14 09:43:14 +0800

    add paidpost-core

--- dev/index.js
+++ dev/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Platform from './plugin/platform.js';
 
+import {
+  PaidPostArticle,
+} from 'paidpost-core';
+
 export default class extends React.Component {
     render() {
         return (
-            <h1>123</h1>
+            <PaidPostArticle>
+                <h1>123</h1>
+            </PaidPostArticle>
+
         );
     }
 }
