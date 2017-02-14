95eeddfba2e582c9001d0d9961595d7b6b2dda7c
ヨシュア <yss901025@gmail.com>
2017-02-14 12:54:41 +0800

    u

--- dev/index.js
+++ dev/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Platform from './plugin/platform.js';
 
-import Demo from './components/Demo';
-
 // import {
 //     PaidPostArticle,
 //     BodyCopy,
 // } from 'paidpost-core';
+
+import Demo from './components/Demo';
 
 export default class extends React.Component {
     render() {
--- dev/main.js
+++ dev/main.js
@@ -6,38 +6,44 @@ import Index from './index.js';
 import './style/_reset.scss';
 
 ReactDOM.render(
-    <div style={{
-        maxWidth: 1605,
-        margin: '0 auto',
-        boxShadow: '-1px 0 4px 0 #e2e2e2',
-        borderRight: '1px solid #e2e2e2',
-        borderLeft: '1px solid #e2e2e2',
-    }}>
-        <div style={{
-            position: 'fixed',
+    <div className="container"
+        style={{
             maxWidth: 1605,
-            height: 118,
-            backgroundColor: 'gray',
-            left: 0,
-            top: 0,
-            right: 0,
             margin: '0 auto',
             boxShadow: '-1px 0 4px 0 #e2e2e2',
             borderRight: '1px solid #e2e2e2',
             borderLeft: '1px solid #e2e2e2',
-            fontSize: 30,
-        }}>header</div>
-
-        <div style={{
-            marginTop: 118,
         }}>
-            <Index />
 
-            <div style={{
+        <div className="header"
+            style={{
+                position: 'fixed',
+                maxWidth: 1605,
                 height: 118,
                 backgroundColor: 'gray',
+                left: 0,
+                top: 0,
+                right: 0,
+                margin: '0 auto',
+                boxShadow: '-1px 0 4px 0 #e2e2e2',
+                borderRight: '1px solid #e2e2e2',
+                borderLeft: '1px solid #e2e2e2',
                 fontSize: 30,
-            }}>footer</div>
+            }}>header</div>
+
+        <div className="wrapper"
+            style={{
+                marginTop: 118,
+            }}>
+
+            <Index />
+
+            <div className="footer"
+                style={{
+                    height: 118,
+                    backgroundColor: 'gray',
+                    fontSize: 30,
+                }}>footer</div>
         </div>
     </div>,
     document.getElementById('app')
