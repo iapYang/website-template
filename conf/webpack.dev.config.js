3f62603ef317cd6d667ec378e6dc4486f3bc0fd1
ヨシュア <yss901025@gmail.com>
2017-03-01 15:33:50 +0800

    dynamic port

--- conf/webpack.dev.config.js
+++ conf/webpack.dev.config.js
@@ -1,14 +1,17 @@
 const merge = require('webpack-merge');
 const webpack = require('webpack');
+const findPortSync = require('find-port-sync');
 
 const baseWebpackConfig = require('./webpack.base.config.js');
+
+const port = findPortSync();
 
 module.exports = merge(baseWebpackConfig, {
     devtool: 'eval-source-map',
     devServer: {
         contentBase: './dev',
         host: 'localhost',
-        port: 9000,
+        port,
         open: true,
         hot: true,
     },
