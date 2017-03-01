ac144777cd109464534b33ad4360c1b6f1135865
ヨシュア <yss901025@gmail.com>
2017-03-01 09:14:21 +0800

    add jsx support

--- conf/webpack.base.config.js
+++ conf/webpack.base.config.js
@@ -19,7 +19,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.js(x)?$/,
                 use: [
                     'babel-loader',
                 ],
